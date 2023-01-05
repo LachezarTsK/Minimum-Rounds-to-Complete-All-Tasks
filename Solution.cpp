1c8547eb10b5e12e3ff2aa6f319414ba233c29d1
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-01-05 04:12:14 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,27 @@
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+    
+    static const int NOT_POSSIBLE_TO_COMPLETE_ALL_TASKS = -1;
+    
+public:
+    //C++20: int minimumRounds(span<const int> tasks) ...
+    int minimumRounds(const vector<int>& tasks) {
+        unordered_map<int, int> taskFrequency;
+        for (int task : tasks) {
+            ++taskFrequency[task];
+        }
+
+        int minRoundsToCompleteAllTasks = 0;
+        for (const auto& [task, frequency] : taskFrequency) {
+            if (frequency == 1) {
+                return NOT_POSSIBLE_TO_COMPLETE_ALL_TASKS;
+            }
+            minRoundsToCompleteAllTasks += ceil(static_cast<double>(frequency) / 3);
+        }
+        return minRoundsToCompleteAllTasks;
+    }
+};
